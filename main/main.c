7ac6ccdc7569d82251394722089f7a664e2a2572
io-tronic <vittenino@gmail.com>
2024-04-25 20:06:20 -0700

    Working Timelapse Function

--- main/main.c
+++ main/main.c
@@ -35,8 +35,15 @@
 #include <sys/param.h>
 #include <string.h>
 
+#include <string.h>
+#include <sys/unistd.h>
+#include <sys/stat.h>
+#include "esp_vfs_fat.h"
+#include "sdmmc_cmd.h"
+
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
+
 
 // support IDF 5.x
 #ifndef portTICK_RATE_MS
@@ -46,55 +53,6 @@
 #include "esp_camera.h"
 
 #define BOARD_XIAO_SENSE 1
-
-// WROVER-KIT PIN Map
-#ifdef BOARD_WROVER_KIT
-
-#define CAM_PIN_PWDN -1  //power down is not used
-#define CAM_PIN_RESET -1 //software reset will be performed
-#define CAM_PIN_XCLK 21
-#define CAM_PIN_SIOD 26
-#define CAM_PIN_SIOC 27
-
-#define CAM_PIN_D7 35
-#define CAM_PIN_D6 34
-#define CAM_PIN_D5 39
-#define CAM_PIN_D4 36
-#define CAM_PIN_D3 19
-#define CAM_PIN_D2 18
-#define CAM_PIN_D1 5
-#define CAM_PIN_D0 4
-#define CAM_PIN_VSYNC 25
-#define CAM_PIN_HREF 23
-#define CAM_PIN_PCLK 22
-
-#endif
-
-// ESP32Cam (AiThinker) PIN Map
-#ifdef BOARD_ESP32CAM_AITHINKER
-
-#define CAM_PIN_PWDN 32
-#define CAM_PIN_RESET -1 //software reset will be performed
-#define CAM_PIN_XCLK 0
-#define CAM_PIN_SIOD 26
-#define CAM_PIN_SIOC 27
-
-#define CAM_PIN_D7 35
-#define CAM_PIN_D6 34
-#define CAM_PIN_D5 39
-#define CAM_PIN_D4 36
-#define CAM_PIN_D3 21
-#define CAM_PIN_D2 19
-#define CAM_PIN_D1 18
-#define CAM_PIN_D0 5
-#define CAM_PIN_VSYNC 25
-#define CAM_PIN_HREF 23
-#define CAM_PIN_PCLK 22
-
-#endif
-
-// ESP32-S3 Sense (Xiao) PIN Map
-#ifdef BOARD_XIAO_SENSE
 
 #define CAM_PIN_PWDN -1  //power down is not used
 #define CAM_PIN_RESET -1 //software reset will be performed
@@ -114,9 +72,36 @@
 #define CAM_PIN_HREF 47
 #define CAM_PIN_PCLK 13
 
-#endif
 
-static const char *TAG = "example:take_picture";
+#define EXAMPLE_MAX_CHAR_SIZE    64
+
+static const char *TAG = "example";
+
+#define MOUNT_POINT "/sdcard"
+
+// Pin assignments can be set in menuconfig, see "SD SPI Example Configuration" menu.
+// You can also change the pin assignments here by changing the following 4 lines.
+#define PIN_NUM_MISO  CONFIG_EXAMPLE_PIN_MISO
+#define PIN_NUM_MOSI  CONFIG_EXAMPLE_PIN_MOSI
+#define PIN_NUM_CLK   CONFIG_EXAMPLE_PIN_CLK
+#define PIN_NUM_CS    CONFIG_EXAMPLE_PIN_CS
+
+static esp_err_t write_bin_file(const char *path, uint8_t *data_ptr, size_t data_len)
+{
+    ESP_LOGI(TAG, "Opening file %s", path);
+    FILE *f = fopen(path, "wb");
+    if (f == NULL) {
+        ESP_LOGE(TAG, "Failed to open file for writing");
+        return ESP_FAIL;
+    }
+    fwrite(data_ptr, data_len, 1, f);
+    fclose(f);
+    ESP_LOGI(TAG, "File written");
+
+    return ESP_OK;
+}
+
+
 
 #if ESP_CAMERA_SUPPORTED
 static camera_config_t camera_config = {
@@ -146,7 +131,7 @@ static camera_config_t camera_config = {
     .pixel_format = PIXFORMAT_JPEG, //YUV422,GRAYSCALE,RGB565,JPEG
     .frame_size = FRAMESIZE_CIF,    //QQVGA-UXGA, For ESP32, do not use sizes above QVGA when not JPEG. The performance of the ESP32-S series has improved a lot, but JPEG mode always gives better frame rates.
 
-    .jpeg_quality = 30, //0-63, for OV series camera sensors, lower number means higher quality
+    .jpeg_quality = 10, //0-63, for OV series camera sensors, lower number means higher quality
     .fb_count = 1,       //When jpeg mode is used, if fb_count more than one, the driver will work in continuous mode.
     .grab_mode = CAMERA_GRAB_WHEN_EMPTY,
 };
@@ -166,8 +151,92 @@ static esp_err_t init_camera(void)
 #endif
 
 void app_main(void)
-{
-#if ESP_CAMERA_SUPPORTED
+{   
+    esp_err_t ret;
+
+    // Options for mounting the filesystem.
+    // If format_if_mount_failed is set to true, SD card will be partitioned and
+    // formatted in case when mounting fails.
+    esp_vfs_fat_sdmmc_mount_config_t mount_config = {
+#ifdef CONFIG_EXAMPLE_FORMAT_IF_MOUNT_FAILED
+        .format_if_mount_failed = true,
+#else
+        .format_if_mount_failed = false,
+#endif // EXAMPLE_FORMAT_IF_MOUNT_FAILED
+        .max_files = 5,
+        .allocation_unit_size = 16 * 1024
+    };
+    sdmmc_card_t *card;
+    const char mount_point[] = MOUNT_POINT;
+    ESP_LOGI(TAG, "Initializing SD card");
+
+    // Use settings defined above to initialize SD card and mount FAT filesystem.
+    // Note: esp_vfs_fat_sdmmc/sdspi_mount is all-in-one convenience functions.
+    // Please check its source code and implement error recovery when developing
+    // production applications.
+    ESP_LOGI(TAG, "Using SPI peripheral");
+
+    // By default, SD card frequency is initialized to SDMMC_FREQ_DEFAULT (20MHz)
+    // For setting a specific frequency, use host.max_freq_khz (range 400kHz - 20MHz for SDSPI)
+    // Example: for fixed frequency of 10MHz, use host.max_freq_khz = 10000;
+    sdmmc_host_t host = SDSPI_HOST_DEFAULT();
+
+    spi_bus_config_t bus_cfg = {
+        .mosi_io_num = PIN_NUM_MOSI,
+        .miso_io_num = PIN_NUM_MISO,
+        .sclk_io_num = PIN_NUM_CLK,
+        .quadwp_io_num = -1,
+        .quadhd_io_num = -1,
+        .max_transfer_sz = 4000,
+    };
+    ret = spi_bus_initialize(host.slot, &bus_cfg, SDSPI_DEFAULT_DMA);
+    if (ret != ESP_OK) {
+        ESP_LOGE(TAG, "Failed to initialize bus.");
+        return;
+    }
+
+    // This initializes the slot without card detect (CD) and write protect (WP) signals.
+    // Modify slot_config.gpio_cd and slot_config.gpio_wp if your board has these signals.
+    sdspi_device_config_t slot_config = SDSPI_DEVICE_CONFIG_DEFAULT();
+    slot_config.gpio_cs = PIN_NUM_CS;
+    slot_config.host_id = host.slot;
+
+    ESP_LOGI(TAG, "Mounting filesystem");
+    ret = esp_vfs_fat_sdspi_mount(mount_point, &host, &slot_config, &mount_config, &card);
+
+    if (ret != ESP_OK) {
+        if (ret == ESP_FAIL) {
+            ESP_LOGE(TAG, "Failed to mount filesystem. "
+                     "If you want the card to be formatted, set the CONFIG_EXAMPLE_FORMAT_IF_MOUNT_FAILED menuconfig option.");
+        } else {
+            ESP_LOGE(TAG, "Failed to initialize the card (%s). "
+                     "Make sure SD card lines have pull-up resistors in place.", esp_err_to_name(ret));
+        }
+        return;
+    }
+    ESP_LOGI(TAG, "Filesystem mounted");
+
+    // Card has been initialized, print its properties
+    sdmmc_card_print_info(stdout, card);
+
+    int img_num = 1;
+
+    //look for images with name image_000.jpg, image_001.jpg, ... in order to determine what the current image_num is
+    struct stat st;
+    const char test_image_file[EXAMPLE_MAX_CHAR_SIZE];
+    while (1)
+    {
+        snprintf(test_image_file, EXAMPLE_MAX_CHAR_SIZE, MOUNT_POINT"/IMAGE%03d.jpg", img_num);
+        
+        ESP_LOGI(TAG, "Checking file %s", test_image_file);
+        if (stat(test_image_file, &st) != 0)
+        {
+            break;
+        }
+        img_num++;
+    }
+
+    ESP_LOGI(TAG, "found %d images", img_num);
 
     while (1)
     {   
@@ -181,18 +250,34 @@ void app_main(void)
 
         // use pic->buf to access the image
         ESP_LOGI(TAG, "Picture taken! Its size was: %zu bytes", pic->len);
+
+        
+        // image file name should be unique based on img_num. image_000.jpg, image_001.jpg, ...
+        const char image_file[EXAMPLE_MAX_CHAR_SIZE];
+        snprintf(image_file, EXAMPLE_MAX_CHAR_SIZE, MOUNT_POINT"/IMAGE%03d.jpg", img_num++);
+
+        // const char* temp = image_file;
+        ret = write_bin_file(image_file, pic->buf, pic->len);
+        if (ret != ESP_OK) {
+            ESP_LOGE(TAG, "Failed to write file : %s", image_file);
+            return;
+        }
+
         esp_camera_fb_return(pic);
 
         if(ESP_OK != esp_camera_deinit()) {
             ESP_LOGI(TAG, "Failed to deinit camera. Exiting");
             return;
         }
-        
 
         vTaskDelay(5000 / portTICK_RATE_MS);
     }
-#else
-    ESP_LOGE(TAG, "Camera support is not available for this chip");
-    return;
-#endif
+
+    // All done, unmount partition and disable SPI peripheral
+    esp_vfs_fat_sdcard_unmount(mount_point, card);
+    ESP_LOGI(TAG, "Card unmounted");
+
+    //deinitialize the bus after all devices are removed
+    spi_bus_free(host.slot);
+
 }
